7d64dd753a304cb744b04597e7525d45641e9391
Abdul Sattar <joindevops2024@gmail.com>
2024-08-29 10:30:17 +0530

    Array consept

--- 06-array.sh
+++ 06-array.sh
@@ -3,7 +3,7 @@
 FRUITS=("APPILE" "kiwi" "orange" "Banana" "graps" "GOWA")
 
 echo "First fruit is :: ${FRUITS[0]}"
-echo "First fruit is :: ${FRUITS[1]}"
-echo "First fruit is :: ${FRUITS[3}"
-echo "First fruit is :: ${FRUITS[4]}"
-echo "First fruit is :: ${FRUITS[@]}"
+echo "secondfruit is :: ${FRUITS[1]}"
+echo "Third fruit is :: ${FRUITS[3]}"
+echo "forth fruit is :: ${FRUITS[4]}"
+echo "all fruit is :: ${FRUITS[@]}"
