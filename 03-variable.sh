326b4aa199a4613d22f496f91930ce078a35ba48
Abdul Sattar <joindevops2024@gmail.com>
2024-08-29 09:53:27 +0530

    variables program

---
+++ 03-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1 =Raheem
+PERSON2 = sattar
+
+echo "$PERSON1::: HI $PERSON2, How are you?"
+echo "$PERSON2::  Hi $PERSON1,i am fine.how are you doing today?"
+echo "$PERSON1 :: i am doing good $PERSON2.whats going on?"
+echo "$PERSON2:: I started learning shell scripting  $PERSON1"
