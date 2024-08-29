85ef92725d20c1e17c74c189618753146227b32b
Abdul Sattar <joindevops2024@gmail.com>
2024-08-29 09:23:12 +0530

    chating with zikriya

---
+++ 02-chating.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Zikriya::: HI Sattar, How are you?"
+echo "Sattar::  Hi zikriya,i am fine.how are you doing today?"
+echo "zikriya :: i am doing good sattar.whats going on?"
+echo "sattar:: I started learning shell scripting  zikriya"
