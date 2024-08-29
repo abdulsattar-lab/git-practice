afa73a07ad5bf802a510324847f18374a2218136
Abdul Sattar <joindevops2024@gmail.com>
2024-08-29 14:15:27 +0530

    updated

--- 04-variable.sh
+++ 04-variable.sh
@@ -6,4 +6,4 @@ PERSON2=$2
 echo "$PERSON1::: HI $PERSON2, How are you?"
 echo "$PERSON2::  Hi $PERSON1,i am fine.how are you doing today?"
 echo "$PERSON1 :: i am doing good $PERSON2.whats going on?"
-echo "$PERSON2:: I started learning shell scripting  $PERSON1"
+echo "$PERSON2:: I started learning shell  scripting  $PERSON1"
