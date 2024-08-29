8bf17f7b2743bdc12aa0032097c235652780488d
Abdul Sattar <joindevops2024@gmail.com>
2024-08-29 10:28:08 +0530

    Array consept

---
+++ 06-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FRUITS=("APPILE" "kiwi" "orange" "Banana" "graps" "GOWA")
+
+echo "First fruit is :: ${FRUITS[0]}"
+echo "First fruit is :: ${FRUITS[1]}"
+echo "First fruit is :: ${FRUITS[3}"
+echo "First fruit is :: ${FRUITS[4]}"
+echo "First fruit is :: ${FRUITS[@]}"
