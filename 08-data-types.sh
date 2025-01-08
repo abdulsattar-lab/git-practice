4fbd0b98428a206161455c21639a0dbc53b5ef8d
Abdul Sattar <joindevops2024@gmail.com>
2025-01-08 20:33:58 +0530

    updated

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-NUMBER1=$1
-NUMBER2=$2
+#NUMBER1=$1
+#NUMBER2=$2
+
+#SUM=$(($NUMBER1+$NUMBER2))
+
+#echo "Total of $NUMBER1 and $NUMBER2 is $SUM" 
+
+NUMBER1=1234
+NUMBER2=2345
 
 SUM=$(($NUMBER1+$NUMBER2))
-
-echo "Total of $NUMBER1 and $NUMBER2 is $SUM" 
+echo "Total of $SUM" 
