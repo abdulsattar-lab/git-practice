ae69a8bfc927f6c71c024dd338d6654a6c4a9ed7
Abdul Sattar <joindevops2024@gmail.com>
2025-01-08 10:15:24 +0530

    Udated

--- 06-array.sh
+++ 06-array.sh
@@ -7,3 +7,10 @@ echo "secondfruit is :: ${FRUITS[1]}"
 echo "Third fruit is :: ${FRUITS[3]}"
 echo "forth fruit is :: ${FRUITS[4]}"
 #echo "all fruit is :: ${FRUITS[@]}"
+
+India=("Andhra" "Telengana" "Tamilnadu" "Kerala" "Karnataka")
+echo "1st state is::${India[0]}"
+echo "2st state is::${India[1]}"
+echo "3st state is::${India[2]}"
+echo "4st state is::${India[3]}"
+echo "5st state is::${India[4]}"
