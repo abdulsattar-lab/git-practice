7c90c828334bbbcfbd3f917d52f2240f2da18cd4
Abdul Sattar <joindevops2024@gmail.com>
2025-01-08 09:57:43 +0530

    03-variable.sh updated

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-PERSON1=Raheem
-PERSON2=sattar
+#PERSON1=Raheem
+#PERSON2=sattar
+user1=Abdul
+user2=Ashik
 
-echo "$PERSON1::: HI $PERSON2, How are you?"
-echo "$PERSON2::  Hi $PERSON1,i am fine.how are you doing today?"
-echo "$PERSON1 :: i am doing good $PERSON2.whats going on?"
-echo "$PERSON2:: I started learning shell scripting  $PERSON1"
+
+#echo "$PERSON1::: HI $PERSON2, How are you?"
+#echo "$PERSON2::  Hi $PERSON1,i am fine.how are you doing today?"
+#echo "$PERSON1 :: i am doing good $PERSON2.whats going on?"
+#echo "$PERSON2:: I started learning shell scripting  $PERSON1"
+
+
+echo "$user1 :: Hi $user2,where are you ..?"
+echo "$user2:: Hey hello $user1, I am Pattikonda Kurnool, Where are you?"
+echo "$user1:: I am in Hyderbad $user2.
