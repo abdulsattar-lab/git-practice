485076508195b3e7e9c767873a5f77478413642d
Abdul Sattar <joindevops2024@gmail.com>
2025-01-08 20:42:59 +0530

    updated

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,9 +2,18 @@
 
 NUMBER=$1
 
-if [ $NUMBER -gt 20 ] #gt.lt.eq.-
+#if [ $NUMBER -gt 20 ] #gt.lt.eq.-
+#then
+#echo "Give number: $NUMBER is  greater then 20"
+#else
+#echo "Given number :$NUMBER is less then 20"
+#fi
+
+number=$1
+
+if[$number -gt 30]
 then
-echo "Give number: $NUMBER is  greater then 20"
+echo "Given $number is greter then 30"
 else
-echo "Given number :$NUMBER is less then 20"
+echo " Given $number is less then 30"
 fi
